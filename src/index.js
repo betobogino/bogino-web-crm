ceaab2f172f63c16be1b1f16a30bc1e2f69b8daf
betobogino <beto.bogino@gmail.com>
2023-04-05 23:47:49 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import app from './app.js';
+import dotenv from 'dotenv';
+import sequelize from './database/database.js';
+
+dotenv.config(); 
+
+//Creo una funcion async para poder usar codigo asincrono para sequelize.authenticate();
+const main = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+        
+        app.listen(process.env.PORT);
+        console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+main();
