9251d00a552151f83c6b51d85d2668074db3194d
betobogino <beto.bogino@gmail.com>
2023-04-15 23:35:43 -0300

    Project completed

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,10 +1,8 @@
 import { Project } from "../models/Project.js";
 
 export const getProjects = async (req, res) => {
-    try{
+    try {
         const projects = await Project.findAll();
-
-        console.log(projects);
     
         res.json(projects);
     } catch (error) {
@@ -12,9 +10,21 @@ export const getProjects = async (req, res) => {
     }
 } 
 
-// const getProject = async = () => {
+export const getProject = async (req, res) => {
+    try {
+        const idProject = req.params.id;
 
-// }
+        const project = await Project.findByPk(idProject)
+
+        if (project === null) {
+            return res.status(404).json({message: 'Projecto no existente'})
+        } else {
+            res.send(project)
+        }
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
 
 export const createProject = async (req, res) => {
     try {
@@ -26,18 +36,50 @@ export const createProject = async (req, res) => {
             priority
         });
 
-        console.log(newProject);
-
         res.json(newProject);    
     } catch (error) {
         res.status(500).json({message: error.message});    
     }
 }
 
-// export const updateProject = async () => {
+export const updateProject = async (req, res) => {
+    try {
+        const idProject = req.params.id;
+        
+        const newData = req.body;
 
-// }
+        let project = await Project.findByPk(idProject)
 
-// export const deleteProject = async () => {
+        if (project === null) {
+            return res.status(404).json({message: 'Projecto no existente'})
+        } else {
+            project.name = newData.name
 
-// }
+            project.save()
+
+            res.send('Proyecto modificado correctamente')
+        }
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
+
+export const deleteProject = async (req, res) => {
+    try {
+        const idProject = req.params.id;
+
+        const deleted = await Project.destroy({
+            where: {
+                id: idProject
+            }
+        })
+
+        if (deleted === 0) {
+            return res.status(404).json({message: 'No se encontro el projecto a eliminar en la base de datos.'})
+        } else {
+            res.sendStatus(204);
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getProjects, createProject } from "../controllers/projects.controller.js";
+import { getProjects, createProject, getProject, updateProject, deleteProject } from "../controllers/project.controller.js";
 
 const router = Router();
 
 router.get('/projects', getProjects);
 router.post('/projects', createProject);
-router.put('/projects/:id');
-router.delete('/projects/:id');
-router.get('/projects/:id');
+router.put('/projects/:id', updateProject);
+router.delete('/projects/:id', deleteProject);
+router.get('/projects/:id', getProject);
 
 export default router;
